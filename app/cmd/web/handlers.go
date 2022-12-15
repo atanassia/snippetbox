c0f2aafdfc24128f49df072fec0435286c092705
Liza Babayan <liza_babayan.ru@mail.ru>
2022-12-15 08:42:29 +0300

    added database

--- app/cmd/web/handlers.go
+++ app/cmd/web/handlers.go
@@ -4,7 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/atanassia/snippetbox/internal/models"
-	"html/template"
+	//"html/template"
 	"net/http"
 	"strconv"
 )
@@ -22,36 +22,9 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 		return
 	}
+
 	for _, snippet := range snippets {
 		fmt.Fprintf(w, "%+v\n", snippet)
-	}
-
-	// Initialize a slice containing the paths to the two files. It's important
-	// to note that the file containing our base template must be the *first*
-	// file in the slice.
-	// files := []string{
-	// 	"./ui/html/layout/base.tmpl",
-	// 	"./ui/html/partials/nav.tmpl",
-	// 	"./ui/html/pages/home.tmpl",
-	// }
-
-	// Use the template.ParseFiles() function to read the template file into a
-	// template set. If there's an error, we log the detailed error message and use
-	// the http.Error() function to send a generic 500 Internal Server Error
-	// response to the user.
-	ts, err := template.ParseFiles(files...)
-	if err != nil {
-		app.errorLog.Println(err.Error())
-		app.serverError(w, err)
-		return
-	}
-
-	// Use the ExecuteTemplate() method to write the content of the "base"
-	// template as the response body.
-	err = ts.ExecuteTemplate(w, "base", nil)
-	if err != nil {
-		app.errorLog.Println(err.Error())
-		app.serverError(w, err)
 	}
 }
 
--- go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/atanassia/snippetbox
-
-go 1.19
